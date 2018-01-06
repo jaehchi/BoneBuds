489ab4110cface9b5b47328995b2811169404c4f
Kevin Vo <kevinvoduy218@gmail.com>
2018-01-06 11:05:36 -0800

    [ edit ] - user profile and index

--- client/src/Content/Routes/UserProfile.jsx
+++ client/src/Content/Routes/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export class UserProfile extends Component{
+class UserProfile extends Component{
   constructor(props) {
     super(props);
     this.state = {
@@ -11,9 +11,16 @@ export class UserProfile extends Component{
   render() {
     console.log('user profile page clicked')
     return (
-      <div>
-        <h1>User Profile Page</h1>
+      <div id="userProfile">
+        <h3>User Profile Page</h3>
+        <p>Farm-to-table prism biodiesel dreamcatcher. Vexillologist vinyl godard offal. Vape flexitarian green juice franzen bitters semiotics, lo-fi vinyl echo park godard pok pok offal meh. Typewriter subway tile meh pickled cronut. Austin coloring book lyft bushwick brunch food truck hashtag lo-fi fanny pack affogato YOLO 90's. Roof party swag palo santo, cold-pressed pabst affogato sartorial intelligentsia freegan biodiesel fanny pack kombucha vexillologist.
+        <br/><br/>
+        Oh. You need a little dummy text for your mockup? How quaint.
+        <br/><br/>
+        I bet you’re still using Bootstrap too…</p>
       </div>
     )
   }
 }
+
+export default UserProfile;
--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const db = require('./SQL/models')
 
 const app = express();
 
+
 app.use(express.static(path.resolve(__dirname, '../client/public')));
+
+// app.get('/', (req, res) => {
+//   console.log('get')
+//   res.send('200')
+// })
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);

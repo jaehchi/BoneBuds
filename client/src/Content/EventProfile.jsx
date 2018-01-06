c2746b3f23f4b0e55a6e12547e790b2906d420a5
Jaehyun Chi <jaehchi@gmail.com>
2018-01-06 11:41:12 -0800

    [add] - "react syntax and synx fixes"

---
+++ client/src/Content/EventProfile.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class EventProfile extends Component {
+  constructor () {
+    super();
+  }
+  componentDidMount () {
+    
+  }
+
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default EventProfile;

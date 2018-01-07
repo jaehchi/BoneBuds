a9cda59f278133d7a2c1c69888ab3f081bfef28c
Kevin Vo <kevinvoduy218@gmail.com>
2018-01-07 03:28:40 -0800

    [ edit ] - rm image upload from page

--- client/src/Content/Routes/UserProfile.jsx
+++ client/src/Content/Routes/UserProfile.jsx
@@ -14,43 +14,40 @@ class UserProfile extends Component{
         <h3>User Profile Page</h3>
         <div class="row">
           <form class="col s12">
-            <div class="file-field input-field">
-              <div class="btn">
-                <span>Upload</span>
-                <input type="file"/>
-              </div>
-              <div class="file-path-wrapper">
-                <input class="file-path validate" type="text"/>
-              </div>
-            </div>
+
             <div class="row">
               <div class="input-field col s6">
                 <input id="first_name" type="text" class="validate"/>
                 <label for="first_name">First Name</label>
               </div>
+
               <div class="input-field col s6">
                 <input id="last_name" type="text" class="validate"/>
                 <label for="last_name">Last Name</label>
               </div>
             </div>
+
             <div class="row">
               <div class="input-field col s12">
                 <input id="disabled" type="text" class="validate"/>
                 <label for="disabled">Address</label>
               </div>
             </div>
+
             <div class="row">
               <div class="input-field col s12">
                 <input id="password" type="password" class="validate"/>
                 <label for="password">Password</label>
               </div>
             </div>
+
             <div class="row">
               <div class="input-field col s12">
                 <input id="email" type="email" class="validate"/>
                 <label for="email" data-error="enter a valid password" data-success="Success!">Email</label>
               </div>
             </div>
+
           </form>
         </div>
       </div>

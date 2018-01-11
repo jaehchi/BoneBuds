fa2497d53fbda3eed1a1ae6907ff9eb1d44aa062
Kevin Vo <kevinvoduy218@gmail.com>
2018-01-11 05:46:53 -0800

    [ minor ux edit ]

--- client/src/Content/Routes/UserEventTile.jsx
+++ client/src/Content/Routes/UserEventTile.jsx
@@ -13,12 +13,12 @@ class UserEventTile extends Component {
   render () {
     return (
       <div className="wrapper">
-      <div className="col s6">
+      <div className="col s4">
         <div className="card">
           <div className="card-image">
-            <img src="https://i.imgur.com/2KoKbtz.gif"/>
-            <span className="card-title">{this.props.event.title}</span>
-            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">add</i></a>
+            <img src="https://rs1107.pbsrc.com/albums/h398/DMNYK1000/LevyTran.gif~c200"/>
+            <span className="card-title black-text"><strong>{this.props.event.title}</strong></span>
+            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">edit</i></a>
           </div>
           <div className="card-content">
             <p><strong>{this.props.event.description}</strong><br/> I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
